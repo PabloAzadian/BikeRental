388bf97a023bbc559880689bd726a098ea5cf110
PabloAzadian <pabloazadian09@gmail.com>
2023-07-29 17:32:28 +0200

    analytics package

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -12,6 +13,7 @@ root.render(
 
   
   <React.StrictMode>
+    <Analytics/>
     <App />
   </React.StrictMode>
 );
